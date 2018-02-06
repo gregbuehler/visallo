890303f76bd11348ec55c8655b9465807dca0b5d
Joey Burke <joeybrk372@gmail.com>
2018-02-06 16:36:43 -0500

    Add default error handler class to web.xml. In dev mode will respond with stack traces for caught exceptions. Otherwise it returns the same content as previous error.html page (generic friendly error message)

---
+++ web/web-base/src/main/java/org/visallo/web/VisalloDefaultErrorHandler.java
@@ -0,0 +1,56 @@
+package org.visallo.web;
+
+import com.google.inject.Injector;
+import org.visallo.core.config.Configuration;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class VisalloDefaultErrorHandler extends HttpServlet {
+    public static final String GENERIC_ERROR_MESSAGE = "Unable to process request. Please contact your system administrator for more details.";
+
+    private boolean devMode;
+
+    @Override
+    public void init() {
+        final Injector injector = (Injector) getServletContext().getAttribute(Injector.class.getName());
+        Configuration config = injector.getInstance(Configuration.class);
+
+        devMode = config.getBoolean(Configuration.DEV_MODE, Configuration.DEV_MODE_DEFAULT);
+    }
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setCharacterEncoding("UTF-8");
+
+        if (devMode) {
+            writeDefaultError(request, response);
+        } else {
+            response.getWriter().write(GENERIC_ERROR_MESSAGE);
+
+        }
+    }
+
+    private void writeDefaultError(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        Exception ex = (Exception) request.getAttribute(RequestDispatcher.ERROR_EXCEPTION);
+
+        if (ex != null) {
+            ex.printStackTrace(response.getWriter());
+        } else {
+            String errorResponse = "Error ";
+            Integer status = (Integer) request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+            String message = (String) request.getAttribute(RequestDispatcher.ERROR_MESSAGE);
+
+            errorResponse += status;
+            if (message != null && message.length() > 0) {
+                errorResponse += " : " + message;
+            }
+
+            response.getWriter().write(errorResponse);
+        }
+
+    }
+}
